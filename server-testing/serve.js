f3774511577f5a6393f3282f0e54bf42cb767181
Harshit Thukral <H.thakral369@gmail.com>
2015-06-21 19:41:39 +0530

    upload file using command curl, show complete upload %

--- server-testing/serve.js
+++ server-testing/serve.js
@@ -4,11 +4,11 @@ var rl = require('readline');
 server.createServer(function(request,response){
 	response.writeHead(200);
 	//var interface = rl.createInterface(process.stdin,process.stdout,null);
-	response.write(function(){
+	response.write(/*function(){
 		interface.question("what's ur name",function(name){
 			console.log("i know his name is " + name + " he is just trying to be oversmart");
 		});	
-	});
+	});*/"hey this is papa jones");
 	response.end();
 }).listen(8080);
 console.log("check your browser on port 8080-----> HINT:use curl");
---
+++ upload-file/file-upload.js
@@ -0,0 +1,22 @@
+var server = require("http");
+var stream = require("fs");
+
+server.createServer(function(request,response){
+	var writefile = stream.createWriteStream("filecopy.html");
+	response.writeHead(200);
+	request.pipe(writefile);
+
+	var filesize = request.headers['content-length'];
+
+	var uploaded = 0;
+
+	request.on('data',function(data){
+		uploaded += data.length;
+		var percent = (uploaded/filesize)*100;
+		response.write("uploaded: " + parseInt(percent,1) + "%\n");
+	})
+
+	request.on('end',function(){
+		response.end("file uploaded");
+	});
+}).listen(8080);
