b832b4d7989b1048e41a82396bc13c450355b9b1
Harshit Thukral <H.thakral369@gmail.com>
2016-01-24 17:21:37 +0530

    some basic changes to make football-api requests

--- express/app-api.js
+++ express/app-api.js
@@ -1,23 +1,31 @@
 var request = require('request');
+var querystring = require('querystring');
 var url = require('url');
 
 var express = require('express');
 
 var app = express();
 
+//---------------------------------------------------
+//standard variables
+
+var host = 'football-api.com';
+var api_key = '50058c89-18bd-95e8-06724d6079b5';
+
 //----------------------------------------------------
 
-app.get('/tweets/:username',function(req,response){
+app.get('/api',function(req,response){
 	var username = req.params.username;
 
 	options = {
-		method: "https:",
-		host: 'api.twitter.com',
-		pathname: '1/statuses/user_timeline.json',
-		query: { screen_name: username,count: 10}
+		method: "GET",
+		host: host,
 	}
 
-	var twitterurl = url.format(options);
+	var apiurl = url.format(options);
+	var apicall = querystring.stringify(apiurl)
+	apicall += '?' + 'Action=competitions&APIKey='+api_key;
+
 
 	request(twitterurl).pipe(response);
 });
